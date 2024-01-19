3a8a7cb5e9e553b3daab91d609c9d1df78be4414
Snanttu <santeri.orava@hotmail.com>
2024-01-19 19:50:33 +0200

    Pants references

--- AnimeSouls/Assets/Scripts/Equipment/Scriptable/Armor_Pants.cs
+++ AnimeSouls/Assets/Scripts/Equipment/Scriptable/Armor_Pants.cs
@@ -7,6 +7,47 @@ public class Armor_Pants : Equipment_Base
 {
     [SerializeField]
     private Sprite[] _pantsSprites;
+
+    [Header("Front")]
+    [SerializeField]
+    private Sprite front_bottom;
+    [SerializeField]
+    private Sprite front_thigh;
+    [SerializeField]
+    private Sprite front_calf;
+
+    [Header("Side")]
+    [SerializeField]
+    private Sprite side_bottom;
+    [SerializeField]
+    private Sprite side_thigh_F;
+    [SerializeField]
+    private Sprite side_thigh_B;
+    [SerializeField]
+    private Sprite side_behind;
+    [SerializeField]
+    private Sprite side_calf;
+
+    [Header("Back")]
+    [SerializeField]
+    private Sprite back_bottom;
+    [SerializeField]
+    private Sprite back_bottom_behind;
+    [SerializeField]
+    private Sprite back_thigh;
+    [SerializeField]
+    private Sprite back_calf;
+
+    [Header("Feet")]
+    [SerializeField]
+    private Sprite foot_front;
+    [SerializeField]
+    private Sprite foot_back;
+    [SerializeField]
+    private Sprite foot_side_F;
+    [SerializeField]
+    private Sprite foot_side_B;
+
     [SerializeField]
     private Sprite[] _masks;
 
