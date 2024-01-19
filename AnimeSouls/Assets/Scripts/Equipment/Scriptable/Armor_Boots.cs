3b26569b2d1eba0dc44bf27a0718353d58132d50
Snanttu <santeri.orava@hotmail.com>
2024-01-19 20:11:04 +0200

    Boots & Gloves references

--- AnimeSouls/Assets/Scripts/Equipment/Scriptable/Armor_Boots.cs
+++ AnimeSouls/Assets/Scripts/Equipment/Scriptable/Armor_Boots.cs
@@ -7,6 +7,35 @@ public class Armor_Boots : Equipment_Base
 {
     [SerializeField]
     private Sprite[] _bootsSprites;
+
+    [Header("Front")]
+    [SerializeField]
+    private Sprite front_calf;
+    [SerializeField]
+    private Sprite front_calf_behind;
+
+    [Header("Side")]
+    [SerializeField]
+    private Sprite side_calf;
+    [SerializeField]
+    private Sprite side_calf_behind;
+
+    [Header("Back")]
+    [SerializeField]
+    private Sprite back_calf;
+    [SerializeField]
+    private Sprite back_calf_behind;
+
+    [Header("Feet")]
+    [SerializeField]
+    private Sprite foot_front;
+    [SerializeField]
+    private Sprite foot_back;
+    [SerializeField]
+    private Sprite foot_side_F;
+    [SerializeField]
+    private Sprite foot_side_B;
+
     [SerializeField]
     private Sprite[] _masks;
 
--- AnimeSouls/Assets/Scripts/Equipment/Scriptable/Armor_Gloves.cs
+++ AnimeSouls/Assets/Scripts/Equipment/Scriptable/Armor_Gloves.cs
@@ -7,6 +7,38 @@ public class Armor_Gloves : Equipment_Base
 {
     [SerializeField]
     private Sprite[] _glovesSprites;
+
+    [SerializeField]
+    private Sprite forearm_behind;
+
+    [Header("Front_R")]
+    [SerializeField]
+    private Sprite front_forearm_R;
+    [SerializeField]
+    private Sprite front_fist_R;
+
+    [Header("Front_L")]
+    [SerializeField]
+    private Sprite front_forearm_L;
+    [SerializeField]
+    private Sprite front_fist_L;
+
+    [Header("Back_R")]
+    [SerializeField]
+    private Sprite back_forearm_R;
+    [SerializeField]
+    private Sprite back_palm_R;
+    [SerializeField]
+    private Sprite back_fingers_R;
+
+    [Header("Back_L")]
+    [SerializeField]
+    private Sprite back_forearm_L;
+    [SerializeField]
+    private Sprite back_palm_L;
+    [SerializeField]
+    private Sprite back_fingers_L;
+
     [SerializeField]
     private Sprite[] _masks;
     [SerializeField]
