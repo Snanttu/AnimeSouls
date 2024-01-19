7fe9bf8fab77ee9f7126c0bc8f4718af9742e651
Snanttu <santeri.orava@hotmail.com>
2024-01-19 23:16:19 +0200

    Girl_Body

---
+++ AnimeSouls/Assets/Scripts/Player/Girl_Body.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Girl_Body : MonoBehaviour
+{
+    [Header("Parts")]
+    [SerializeField]
+    private Girl_Head head;
+    [SerializeField]
+    private Girl_Torso torso;
+    [SerializeField]
+    private Girl_Arm arm_front;
+    [SerializeField]
+    private Girl_Arm arm_back;
+    [SerializeField]
+    private Girl_Hand hand_front;
+    [SerializeField]
+    private Girl_Hand hand_back;
+    [SerializeField]
+    private Girl_Leg leg_front;
+    [SerializeField]
+    private Girl_Leg leg_back;
+
+    public void ChangeShirt(Sprite[] sprites, Sprite[] masks)
+    {
+
+    }
+
+    public void ChangeGloves(Sprite[] sprites, Sprite[] masks)
+    {
+
+    }
+
+    public void ChangePants(Sprite[] sprites, Sprite[] masks)
+    {
+
+    }
+
+    public void ChangeBoots(Sprite[] sprites, Sprite[] masks)
+    {
+
+    }
+}
